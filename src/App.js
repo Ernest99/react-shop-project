d08d7d337eeaeb06d3d2e89a3841d62b0655e1ba
vibrant_coder <74645353+Ernest99@users.noreply.github.com>
2022-07-21 22:39:41 +0000

    delated scrolltotop

--- src/App.js
+++ src/App.js
@@ -5,14 +5,14 @@ import Shop from "./components/Shop";
 import Feature from "./components/Feature";
 import About from "./components/About";
 import Contact from "./components/Contact";
-import ScrollToTop from "./components/ScrollToTop";
+
 
 
 function App() {
   return (
    <>
    <Routes>
-    <ScrollToTop/>
+   
     <Route path="/" element={
       <>
       <Header/>
