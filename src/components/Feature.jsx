dcf236c6b96f162289c1b523dfdae7d1d98c18c8
vibrant_coder <74645353+Ernest99@users.noreply.github.com>
2022-07-28 14:59:41 +0000

    added feature component

--- src/components/Feature.jsx
+++ src/components/Feature.jsx
@@ -1,13 +1,46 @@
 import React from 'react'
 import Nav from './Nav'
+import './feature.css'
+import fImage from '../components/assets/f4.jpg'
+import Items from './Items'
+import Footer from '../components/Footer'
 
 const Feature = () => {
   return (
     <div>
         <Nav/>
         <section>
-        <h1>Feature</h1>
+        <div className="feature_container">
+          <div className="f_left">
+            <div className="f_img">
+              <img src={fImage} alt="" />
+            </div>
+          </div>
+          <div className="f_right">
+            <h4>Home/T-Shirt</h4>
+            <h2>Men's Zicon Top</h2>
+            <h3>₵15.50</h3>
+            <div className="option">
+              <select>
+                <option value="Select Size">Select Size</option>
+                <option value="Small">S</option>
+                <option value="Medium">M</option>
+                <option value="Large">L</option>
+                <option value="Xtra Large">XL</option>
+              </select>
+            </div>
+            <div className="quantity">
+              <input type="number" placeholder='1'/>
+              <button>Add To Cart</button>
+            </div>
+            <h2 className='product_details'>Product Details</h2>
+            <p>The Zicon shirt checks in with an all-white rose design. This 100% cotton white short-sleeve T-shirt is highlighted by small milky flowers on the chest and is completed by a classic round neck.</p>
+          </div>
+        </div>
+        <Items/>
         </section>
+        
+        <Footer/>
     
     </div>
   )
