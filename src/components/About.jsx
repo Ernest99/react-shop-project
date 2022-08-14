869649b4ee8af1e2ad2cdb912aa4ad36d11283d7
vibrant_coder <74645353+Ernest99@users.noreply.github.com>
2022-08-14 18:06:35 +0000

    added content to about component

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import Nav from './Nav'
 import './about.css'
-
+import aboutImg from '../components/assets/1.jpg'
+import Footer from './Footer'
+import Btn from './Btn'
 const About = () => {
   return (
     <div>
@@ -9,6 +11,24 @@ const About = () => {
         <div className="about">
           <h1>#ABOUT</h1>
         </div>
+        <section>
+          <div className="about_section">
+            <div className="about_">
+              <img src={aboutImg} alt="" />
+            </div>
+            <div className="about_r">
+              <h1>About Us</h1>
+              <div className="bio">
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Pariatur, nemo eveniet deserunt voluptatibus aliquam culpa excepturi inventore quidem eaque sequi. Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor delectus culpa esse veniam sit saepe ullam repudiandae enim distinctio debitis rerum nostrum ea nulla laboriosam natus, iste exercitationem? Possimus officiis beatae quis quia quae voluptas hic nihil sint fuga vel?</p>
+                <div className="about_btn">
+                <Btn text={'Start Shopping Now'} className='btn-shop'/>
+                </div>
+                
+              </div>
+            </div>
+          </div>
+        </section>
+        <Footer/>
     </div>
   )
 }
