0590ec46e6f1194a5dab9059b10d1b5a1884e045
vibrant_coder <74645353+Ernest99@users.noreply.github.com>
2022-08-25 19:00:16 +0000

    added contact content

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import Nav from './Nav'
 import './contact.css'
+import * as BsIcon from 'react-icons/bs'
+import Footer from './Footer'
 
 const Contact = () => {
   return (
@@ -9,7 +11,48 @@ const Contact = () => {
         <div className="contact">
         <h1>#CONTACT</h1>
         </div>
-        
+        <section>
+          <div className="contact_container">
+            <div className="contact_l">
+           <div className="box">
+            <BsIcon.BsMessenger/>
+            <h3>Messenger</h3>
+            <h5>Alpha Store</h5>
+           </div>
+           <div className="box">
+            <BsIcon.BsWhatsapp/>
+            <h3>WhatsApp</h3>
+            <h5>0265429443</h5>
+           </div>
+           <div className="box">
+            <BsIcon.BsInstagram/>
+            <h3>Instagram</h3>
+            <h5>Alpha_Store</h5>
+           </div>
+            </div>
+            <div className="contact_r">
+              <form action="#">
+                <div className="input_field">
+                  <label>Name</label>
+                  <input type="text" placeholder='Enter Your Name'/>
+                </div>
+                <div className="input_field">
+                  <label>Email</label>
+                  <input type="email" placeholder='Enter Your Email'/>
+                </div>
+                <div className="input_field">
+                  <label>Message</label>
+                <textarea placeholder='Send a Comment' rows="4"></textarea>
+                </div>
+                <div className='contact_btn'>
+                <button>Submit</button>
+                </div>
+              
+              </form>
+            </div>
+          </div>
+        </section>
+        <Footer/>
     </div>
   )
 }
